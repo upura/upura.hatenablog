1829d150ea6342f8dd783fca1a4898fb25ebf2f7
upura <upura0@gmail.com>
2017-09-02 22:59:48 +0900

    Add first_hand_init function

--- duellinks/metebura-auto.py
+++ duellinks/metebura-auto.py
@@ -3,6 +3,9 @@
 Created on Sat Sep  2 22:27:00 2017
 """
 
+import numpy as np
+import random
+
 # deck_list
 deck_list = []
 card_list = {"FUSION_CARD":6,
@@ -17,4 +20,12 @@ def deck_list_init():
         for i in range(card_list[card]):
             deck_list.append(card)
 
+def first_hand_init():
+    if np.random.rand() > 0.5:
+        first_hand_num = 4
+    else:
+        first_hand_num = 5
+    return random.sample(deck_list, first_hand_num)
+
 deck_list_init()
+first_hand = first_hand_init()
