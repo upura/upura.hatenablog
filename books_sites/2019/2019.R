a3018a2e4fc3daef17b0068270edd9cc77ec9bce
upura <upura0@gmail.com>
2018-12-27 21:01:12 +0900

    update

--- books_sites/2019/2019.R
+++ books_sites/2019/2019.R
@@ -1,13 +1,20 @@
+is_prime(2019)
+
+library(gmp)
+factorize(2019)
+
+17^2 + 19^2 + 37^2
+
 library(primes)
 x = generate_primes(max = 40)
-x = append(x, c(1))
-df = combn(x, m=3)
-table(apply(df, 2, function(x) {return (sum(x^2))}))
+xcombn = combn(x, m=3)
+results = apply(
+  xcombn, 2, function(x) {return (sum(x^2))}
+)
+table(results)
 
 y = c(17, 19, 37)
 sum(y^2)
 
 z = c(11, 23, 37)
 sum(z^2)
-
-apply(expand.grid(x, x, x), 1, function(x) {return (sum(x^2))})
